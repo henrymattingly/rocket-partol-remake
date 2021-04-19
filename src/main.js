6ed9bda0ead34c542e0508c42841788ab22f67c7
Henry Mattingly <hsmattin@ucsc.edu>
2021-04-19 11:23:49 -0700

    added second player, created/found new sounds and assets

--- src/main.js
+++ src/main.js
@@ -13,3 +13,23 @@ let borderPadding = borderUISize / 3;
 
 // reserve keyboard variables
 let keyF, keyR, keyLEFT, keyRIGHT, keyD, keyA, keyW;
+
+/*
+Henry Mattingly
+Rocket Patrol Remake modding assignment
+date :
+time spent : ~10 hours
+
+changes made:
+-added a simultaneous second player (co-op)(30)
+-changed the theme of the game, artwork, UI, and music (from sci-fi to foxs in the grass)(60)
+-made a different bullet sprite that moves faster, different size, 
+    and shoots faster with a different sound new weapon (player 1's weapon) (20)
+-created a new scrolling sprite background()
+-added new copyright free backgorund music (5)   https://www.youtube.com/watch?v=etlnsi-Kkac
+-new scrolling background (5)
+-new sprite for spaceship enemies (10)
+-new weapon (player 1's weapon) (20)
+
+
+*/
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -6,9 +6,10 @@ class Menu extends Phaser.Scene {
   preload() {
       // load audio
       this.load.audio('sfx_select', './assets/blip_select12.wav');
-      this.load.audio('sfx_explosion', './assets/explosion38.wav');
+      this.load.audio('sfx_explosion', './assets/meow.wav');
       this.load.audio('sfx_rocket', './assets/rocket_shot.wav');
       this.load.audio('sfx_music', './assets/FarmMusic.wav');
+      this.load.audio('sfx_bullet', './assets/bulletSound.mp3');
   }
 
   create() {
@@ -28,10 +29,10 @@ class Menu extends Phaser.Scene {
       
       // show menu text
       this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-      this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (F) to fire \nUse A & D to move and W to fire', menuConfig).setOrigin(0.5);
       menuConfig.backgroundColor = '#00FF00';
       menuConfig.color = '#000';
-      this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+      this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding + 30, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
 
       // define keys
       keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
